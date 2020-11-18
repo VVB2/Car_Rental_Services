ca2ce3e306fb744a97ce7d1c519c723531f7f301
Vinod Bhat <Vinodvamanbhat@gmail.com>
2020-11-18 12:12:41 +0530

    Initial commit

---
+++ src/car_rental_services/Car_Rental_Services.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car_rental_services;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Admin
+ */
+public class Car_Rental_Services extends Application {
+    private double x,y;
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("LandingPage.fxml"));
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+    }
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/car_rental_services/LandingPageController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car_rental_services;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class LandingPageController implements Initializable {
+    
+    @FXML
+    private BorderPane borderpane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void minimize(MouseEvent event) {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+
+    @FXML
+    private void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    
+    @FXML
+    private void signinbtn(MouseEvent event) {
+        loadUI("SignIn.fxml");
+    }
+    
+    private void loadUI(String ui){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(ui));   
+        } catch(IOException ex) {
+            Logger.getLogger(LandingPageController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        borderpane.setCenter(root);
+    }
+}
